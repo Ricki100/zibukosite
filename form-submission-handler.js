4b8d98e8755723768ec1529412a0df22afbcff86
Ricardo Chitagu <ricardo.everest@yahoo.com>
2020-06-17 22:46:16 +0200

    further validation

--- form-submission-handler.js
+++ form-submission-handler.js
@@ -25,6 +25,50 @@ $('#form').submit(function(e) {
       timer: 4000
   })
   $( "#button" ).prop( "disabled", true );
+  $( "input" ).prop( "disabled", true );
   $( "#button" ).css( {"background-color":"red"},{"border":"0px"});
   }
   
+
+  //emailvalidation//
+function ValidateEmail(inputText)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+document.form1.email.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.email.focus();
+return false;
+}
+}
+
+
+function ValidateEmail() {
+	var email = document.getElementById("email").value;
+	var lblError = document.getElementById("lblError");
+	lblError.innerHTML = "";
+	var expr = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+	if (!expr.test(email)) {
+		lblError.innerHTML = "Invalid email address.";
+	}
+}
+
+
+$('.amount').each(function () {
+  var item = $(this).text();
+  var num = Number(item).toLocaleString('en');    
+
+  if (Number(item) < 0) {
+      num = num.replace('-','');
+      $(this).addClass('negMoney');
+  }else{
+      $(this).addClass('enMoney');
+  }
+  
+  $(this).text(num);
+});
