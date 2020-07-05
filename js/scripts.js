ba905a091d06856fe993c6d8319ae20121b723c4
Ricardo Chitagu <ricardo.everest@yahoo.com>
2020-07-05 18:58:10 +0200

    contact form

--- js/scripts.js
+++ js/scripts.js
@@ -411,6 +411,20 @@ function startSlideshow(){
         $(".next-of-kin").hide();
 
       });
+
+      function openCity(evt, cityName) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(cityName).style.display = "block";
+        evt.currentTarget.className += " active";
+      }
   
 
 
